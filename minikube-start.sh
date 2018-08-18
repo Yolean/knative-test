6f8715b9a6ec7a08260d23aa3809c3f649024f33
Staffan Olsson <staffan@repos.se>
2018-08-18 16:36:12 +0200

    +2 GB to fit single-node kubernetes-kafka too

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,6 +1,6 @@
-minikube start --memory=8192 --cpus=4 \
-  --kubernetes-version=v1.10.5 \
-  --vm-driver=kvm2 \
+minikube start --memory=10240 --cpus=4 \
+  --kubernetes-version=v1.10.6 \
+  --vm-driver=hyperkit \
   --bootstrapper=kubeadm \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
   --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
